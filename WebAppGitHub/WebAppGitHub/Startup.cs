33b9a95b370766009aab55ffd4f3cd42990d1e27
JANO <JANO@JAN>
2016-11-21 11:25:39 +0100

    Hello maaaan

---
+++ WebAppGitHub/WebAppGitHub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppGitHub.Startup))]
+namespace WebAppGitHub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
